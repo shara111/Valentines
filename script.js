22573c4d20a721ee6e87ac35576b3e2b8462f7e3
Sean Muniz <108829819+seanMuniz@users.noreply.github.com>
2025-02-08 22:11:19 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const noButton = document.getElementById('no');
+const yesButton = document.getElementById('yes')
+noButton.addEventListener('click', function () {
+        // Generate random positions within the viewport
+        const maxX = window.innerWidth - noButton.clientWidth;
+        const maxY = window.innerHeight - noButton.clientHeight;
+    
+        const randomX = Math.random() * maxX;
+        const randomY = Math.random() * maxY;
+    
+        // Apply new position
+        noButton.style.position = 'absolute'; 
+        noButton.style.left = `${randomX}px`;
+        noButton.style.top = `${randomY}px`;
+});
+
+yesButton.addEventListener('click', function () {
+    alert('Hooray! Love you lots baby. Get ready for our date today @ 6:30pm');
+});
